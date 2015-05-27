984ff52e20dff483ffeb671f5949372dbfe576f7
cgcgbcbc <cgcgbcbc@163.com>
2015-05-27 16:10:07 +0800

    refactor

--- main.go
+++ main.go
@@ -3,18 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
+	"log"
 	"os"
 	"strings"
 	"text/template"
 )
 
+const templateStringErrorTemplate = `
+render template string failed
+
+template:
+%s
+
+data:
+%s
+
+reason:
+%s
+`
+const templateFileErrorTemplate = `
+render template file
+
+file:
+%s
+
+data:
+%s
+
+reason:
+%s
+`
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "gorender"
 	app.Usage = "render go template on the fly"
-
-	var templateString string
-	var templatePath string
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
@@ -27,56 +50,16 @@ func main() {
 		},
 	}
 
-	app.Action = func(c *cli.Context) {
-		templateString = c.String("string")
-		templatePath = c.String("path")
-		if templatePath != "" && templateString != "" {
-			fmt.Println("do not use both string template and template file")
-			return
-		}
-		if templatePath == "" && templateString == "" {
-			fmt.Println("missing template")
-			return
-		}
-		data := map[string]string{}
-		for _, x := range c.Args() {
-			i := strings.Index(x, "=")
-			if i == -1 {
-				fmt.Println("not a key value pair key=value,", x)
-				return
-			}
-			key := x[:i]
-			value := x[i+1:]
-			if key == "" {
-				fmt.Println("missing key,", x)
-				return
-			}
-			data[key] = value
-		}
-		if templateString != "" {
-			err := renderStringTemplate(templateString, data)
-			if err != nil {
-				panic(err)
-			}
-		}
-		if templatePath != "" {
-			err := renderFileTemplate(templatePath, data)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-
+	app.Action = action
 	app.Run(os.Args)
 }
 
 func renderStringTemplate(templateString string, data map[string]string) (err error) {
-	tmpl, err := template.New("string").Parse(templateString)
+	tmpl, err := template.New("").Parse(templateString)
 	if err != nil {
 		return
 	}
-	err = tmpl.Execute(os.Stdout, data)
-	return
+	return render(tmpl, data)
 }
 
 func renderFileTemplate(templatePath string, data map[string]string) (err error) {
@@ -84,6 +67,62 @@ func renderFileTemplate(templatePath string, data map[string]string) (err error)
 	if err != nil {
 		return
 	}
-	err = tmpl.Execute(os.Stdout, data)
+	return render(tmpl, data)
+}
+
+func render(t *template.Template, data map[string]string) (err error) {
+	err = t.Execute(os.Stdout, data)
 	return
+}
+
+func validateParameter(templateString string, templatePath string) (err error) {
+	if templatePath != "" && templateString != "" {
+		return fmt.Errorf("do not use both string template and template file")
+	}
+	if templatePath == "" && templateString == "" {
+		return fmt.Errorf("missing template")
+	}
+	return nil
+}
+
+func constructData(data *map[string]string, args cli.Args) (err error) {
+	for _, x := range args {
+		i := strings.Index(x, "=")
+		if i == -1 {
+			return fmt.Errorf("not a key value pair key=value: %s", x)
+		}
+		key := x[:i]
+		value := x[i+1:]
+		if key == "" {
+			return fmt.Errorf("missing key for value %s", x)
+		}
+		(*data)[key] = value
+	}
+	return nil
+}
+
+func action(c *cli.Context) {
+	templateString := c.String("string")
+	templatePath := c.String("path")
+	err := validateParameter(templateString, templatePath)
+	if err != nil {
+		log.Fatalf("parameter is invalid, reason: %s", err)
+	}
+	data := map[string]string{}
+	err = constructData(&data, c.Args())
+	if err != nil {
+		log.Fatalf("data is invalid, reason: %s", err)
+	}
+	if templateString != "" {
+		err := renderStringTemplate(templateString, data)
+		if err != nil {
+			log.Fatalf(templateStringErrorTemplate, templateString, data, err)
+		}
+	}
+	if templatePath != "" {
+		err := renderFileTemplate(templatePath, data)
+		if err != nil {
+			log.Fatalf(templateFileErrorTemplate, templatePath, data, err)
+		}
+	}
 }
